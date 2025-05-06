2c0d4d15e814a6ece4b037c69875a5222808a3f1
ratnaphp <ra7709702@gmail.com>
2025-05-06 09:20:36 +0600

    first commit

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php  
-echo "<h1>Arrays</h1>"
-echo "<h3>Indexed Array (Numerical Keys)</h3>"
+echo "<h1>Arrays</h1>";
+echo "<h3>Indexed Array (Numerical Keys)</h3>";
 $colors = array("red", "green", "blue");
 
 
 
-echo "<h3>Associative Array (Named Keys)</h3>"
+echo "<h3>Associative Array (Named Keys)</h3>";
 $person = array("name" => "John", "age" => 30, "city" => "New York");
 
 
-echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>"
+echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>";
 $users = array(
-    array("name" => "Alice", "email" => "alice@example.com"),
-    array("name" => "Bob", "email" => "bob@example.com")
+    array("name" => "Alice", "email" => "alice@example.com");
+    array("name" => "Bob", "email" => "bob@example.com");
 
+    echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>";
 
-
+    $fruits = array("apple", "banana", "cherry");
 
 ?>
