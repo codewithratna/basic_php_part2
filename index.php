7fe3ac3f432b8f2acbc7b764069e78489362c476
ratnaphp <ra7709702@gmail.com>
2025-05-06 09:05:59 +0600

    Array

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php  
+echo "<h1>Arrays</h1>"
+echo "<h3>Indexed Array (Numerical Keys)</h3>"
+$colors = array("red", "green", "blue");
+
+
+
+echo "<h3>Associative Array (Named Keys)</h3>"
+$person = array("name" => "John", "age" => 30, "city" => "New York");
+
+
+echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>"
+$users = array(
+    array("name" => "Alice", "email" => "alice@example.com"),
+    array("name" => "Bob", "email" => "bob@example.com")
+
+
+
+
+?>
