b79e12b103ef91269252e60e3934dd7dd2db3b12
ratnaphp <ra7709702@gmail.com>
2025-05-10 15:12:44 +0600

    last array

--- index.php
+++ index.php
@@ -1,36 +1,13 @@
 <?php  
 echo "<h1>Arrays</h1>";
-echo "<h3>Indexed Array (Numerical Keys)</h3>";
-$colors = array("red", "green", "blue");
+echo "<h3>Indexed Array Numerical Keys</h3>";
 
-
-
-echo "<h3>Associative Array (Named Keys)</h3>";
-$person = array("name" => "Ratna", "age" => 26, "city" => "New York");
-
-
-echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>";
-
-$users = array(
-    array("name" => "Alice", "email" => "alice@example.com"),
-    array("name" => "Bob", "email" => "bob@example.com")
-);
-
-   echo "<h3>Using array function</h3>";
-
-    $fruits = array("apple", "banana", "cherry");
-
-    $fruits = array("apple", "banana", "cherry");
-
-     echo $fruits[0]; //apple
-     echo $person['name'];  //Ratna
-     echo $users[1]['email'];  //ratna@gmail.com
 
      //array holo ekta variable jar moddhe onk gula man rakha jay
 
-     $name1 = "Ratna"
-     $name1 = "Jannat"
-     $name1 = "Fatema"
+     $name1 = "Ratna";
+     $name2 = "Jannat";
+     $name3 = "Fatema";
 
     //eta puraton poddhoti ja sob version e kora jay
      $names = array ("Ratna", "Jannat", "Fatema");
@@ -38,8 +15,38 @@ $users = array(
      /*eta new poddhoti  PHP version 5.4 ba 
      tarporer version gula te hoy*/
 
-     $name2 = ["munia", "moni", "mukta"]
-     
+     $name = ["munia", "moni", "mukta"];
+
+     //kivabe array theke data ber korbo
+
+     echo $name[0]; 
+     echo "<br>";
+     echo $names[1];
+     echo "<br>";
+
+     //kivabe array er kono item change korte hoy
+     $name[2] = "Tour e jabo";
+
+     echo $name[2];
+
+     //new value kivabe add korbo
+     $name[] = "Shylet";
+
+    //shese add hobe Shylet
+     echo $name[3];
+
+     unset($name3);
+
+     Function	
+count($name)	                  //মোট কয়টা item আছে
+array_values($name)	             //শুধু values return করে
+array_keys($name)	            //index/key গুলো return করে
+in_array("munia", $name)	    //munia আছে কিনা check করে
+array_key_exists(1, $name)      //index/key 1 আছে কিনা check করে
+    
+
+
+
 
 
     
