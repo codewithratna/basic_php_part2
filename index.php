4677598e5b58ea26cdb1da66bc28dd66a73750aa
ratnaphp <ra7709702@gmail.com>
2025-05-10 14:44:58 +0600

    arry

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $colors = array("red", "green", "blue");
 
 
 echo "<h3>Associative Array (Named Keys)</h3>";
-$person = array("name" => "John", "age" => 30, "city" => "New York");
+$person = array("name" => "Ratna", "age" => 26, "city" => "New York");
 
 
 echo "<h3>Multidimensional Array (Arrays within Arrays)</h3>";
@@ -20,4 +20,27 @@ $users = array(
 
     $fruits = array("apple", "banana", "cherry");
 
+    $fruits = array("apple", "banana", "cherry");
+
+     echo $fruits[0]; //apple
+     echo $person['name'];  //Ratna
+     echo $users[1]['email'];  //ratna@gmail.com
+
+     //array holo ekta variable jar moddhe onk gula man rakha jay
+
+     $name1 = "Ratna"
+     $name1 = "Jannat"
+     $name1 = "Fatema"
+
+    //eta puraton poddhoti ja sob version e kora jay
+     $names = array ("Ratna", "Jannat", "Fatema");
+
+     /*eta new poddhoti  PHP version 5.4 ba 
+     tarporer version gula te hoy*/
+
+     $name2 = ["munia", "moni", "mukta"]
+     
+
+
+    
 ?>
